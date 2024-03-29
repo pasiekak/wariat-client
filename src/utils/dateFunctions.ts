6c03e94ec01fe428f7e09c84a84bda26296e94a1
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2024-03-29 11:33:55 +0100

    getNowDateToInputString : new function converting date to input type="date" string parseToInputDateString : new function to parse date to input type="date" string

--- src/utils/dateFunctions.ts
+++ src/utils/dateFunctions.ts
@@ -1,8 +1,21 @@
-export const getNowDateToInputString = () => {
+export const getNowDateTimeToInputString = () => {
   let now = new Date();
   const offset = now.getTimezoneOffset();
   now = new Date(now.getTime() - offset * 60 * 1000);
   return now.toISOString().slice(0, 16);
+};
+
+export const getNowDateToInputString = () => {
+  let now = new Date();
+  const offset = now.getTimezoneOffset();
+  now = new Date(now.getTime() - offset * 60 * 1000);
+  return now.toISOString().slice(0, 10);
+};
+
+export const parseToInputDateString = (date: Date) => {
+  const offset = date.getTimezoneOffset();
+  date = new Date(date.getTime() - offset * 60 * 1000);
+  return date.toISOString().slice(0, 10);
 };
 
 export const isFutureDate = (dateString: string): boolean => {
