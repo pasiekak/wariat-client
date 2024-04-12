319970ac03cd665ccdc983d1e8cad527b1395370
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2024-04-12 15:11:05 +0200

    new functions to calculate prices

--- src/utils/priceFunctions.ts
+++ src/utils/priceFunctions.ts
@@ -8,4 +8,31 @@ const calculateNetto = (brutto: number) => {
   return Number((brutto / 1.23).toFixed(2));
 };
 
-export { calculateNetto, calculateBrutto };
+const calculateFinalPrice = (
+  brutto: number,
+  percentage: number,
+  quantity: number,
+) => {
+  if (percentage > 0)
+    return parseFloat(
+      ((brutto - brutto * (percentage / 100)) * quantity).toFixed(2),
+    );
+  return parseFloat((brutto * quantity).toFixed(2));
+};
+
+const calculateDifference = (
+  brutto: number,
+  percentage: number,
+  quantity: number,
+) => {
+  const normalPrice = calculateFinalPrice(brutto, 0, quantity);
+  const priceWithDiscount = calculateFinalPrice(brutto, percentage, quantity);
+  return parseFloat((normalPrice - priceWithDiscount).toFixed(2));
+};
+
+export {
+  calculateNetto,
+  calculateBrutto,
+  calculateFinalPrice,
+  calculateDifference,
+};
