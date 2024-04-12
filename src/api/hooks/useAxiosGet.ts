ff68bcbe2c602e0bca0b75f768b6c658a902ea61
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2024-04-12 15:09:08 +0200

    generics

--- src/api/hooks/useAxiosGet.ts
+++ src/api/hooks/useAxiosGet.ts
@@ -1,32 +1,16 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { IUser } from "../../features/dashboard/features/users/types/IUser";
-import { singleAttribute } from "../types/singleAttribute";
-import { IRole } from "../types/IRole";
-import { ICategory } from "../types/ICategory";
-import { IProduct } from "../types/IProduct";
-import { IProductsItems } from "../../features/dashboard/types/items";
 
 type useAxiosGetProps = {
   url: string;
 };
-type returnedObject = {
-  data: null | {
-    success: boolean;
-    message: string;
-    user?: IUser;
-    users?: IUser[];
-    categories: ICategory[];
-    products: IProduct[];
-    singleAttribute?: singleAttribute;
-    body?: IRole[];
-    items: IProductsItems;
-  };
+type returnedObject<T> = {
+  data: T | null;
   error: string;
   loading: boolean;
 };
 
-const useAxiosGet = ({ url }: useAxiosGetProps): returnedObject => {
+const useAxiosGet = <T>({ url }: useAxiosGetProps): returnedObject<T> => {
   const [data, setData] = useState(null);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
