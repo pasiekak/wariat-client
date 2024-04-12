133f15ad148f8fa3caca9466dc1251529827cab4
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2024-04-12 15:09:15 +0200

    props change

--- src/api/hooks/product/useProduct.ts
+++ src/api/hooks/product/useProduct.ts
@@ -2,29 +2,24 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { IProduct } from "../../types/IProduct";
 
-type useCategoryProps = {
-  productID?: number;
-};
-
 type returnedObject = {
   data: null | {
     success: boolean;
     message: string;
-    products?: IProduct[];
     product?: IProduct;
   };
   error: string;
   loading: boolean;
 };
 
-const useProduct = ({ productID }: useCategoryProps = {}): returnedObject => {
+const useProduct = (productID: number): returnedObject => {
   const [data, setData] = useState(null);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
-      .get(productID ? `/api/products/${productID}` : "/api/products")
+      .get(`/api/products/${productID}`)
       .then((response) => setData(response.data))
       .catch((error) => setError(error.message))
       .finally(() => setLoading(false));
