1189d3700cfa3d01958a254e06d4b3ab046383ac
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2024-04-30 21:53:44 +0200

    Not logged auth pages now redirect logged user to home page

--- src/features/routing/components/NotLoggedOnlyRoutes.tsx
+++ src/features/routing/components/NotLoggedOnlyRoutes.tsx
@@ -5,7 +5,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const NotLoggedOnlyRoutes = () => {
   const { isLogged } = useContext(AccountContext);
 
-  return !isLogged() ? <Outlet /> : <Navigate to="/account" />;
+  return !isLogged() ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default NotLoggedOnlyRoutes;
