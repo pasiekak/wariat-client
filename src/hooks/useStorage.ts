5a293d29e6aecff5c1e98e3e5f17f3616d89ef65
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2024-04-30 12:11:22 +0200

    Fix for non-json values

--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -18,7 +18,13 @@ function useStorage<T>(
 ): [T, Dispatch<SetStateAction<T>>] {
   const [value, setValue] = useState<T>(() => {
     const jsonValue = storageObject.getItem(key);
-    if (jsonValue != null) return JSON.parse(jsonValue);
+    if (jsonValue != null) {
+      try {
+        return JSON.parse(jsonValue);
+      } catch (e) {
+        return jsonValue;
+      }
+    }
 
     if (typeof defaultValue === "function") {
       return defaultValue();
