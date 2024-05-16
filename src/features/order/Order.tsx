3f7efd03e51e4bff0b2c66023b25431e3b1c8efb
Krzysztof Pasieka <krzysztof.pasieka00@gmail.com>
2024-05-16 15:53:35 +0200

    Cart summary is not visible now in order steps. It's visible only in order summary

--- src/features/order/Order.tsx
+++ src/features/order/Order.tsx
@@ -2,14 +2,12 @@ import { Outlet } from "react-router-dom";
 import Navigation from "./components/Navigation";
 
 import "./styles/styles";
-import QuickSummary from "../cart/features/quick-summary/QuickSummary";
 
 const Order = () => {
   return (
     <section className="order bck-smooth">
       <Navigation />
       <Outlet />
-      <QuickSummary type={"in-order"} withColumns={false} />
     </section>
   );
 };
